97cf0cdc0623f39f98339cdde50508f846c5a7ae
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-16 19:42:08 +0900

    Update first_221226.sh

--- first_221226.sh
+++ first_221226.sh
@@ -3,14 +3,16 @@
 echo installing package 21/03/27
 
 << COMMENTOUT
+
+# SSH Server
 echo 1896 | sudo -S apt install openssh-server -y
 sudo systemctl start ssh
 sudo systemctl enable ssh
 
+# 壁紙チェンジャー
+sudo apt install variety
+sudo apt update
 COMMENTOUT
 
-# 壁紙チェンジャー
-sudo add-apt-repository ppa:peterlevi/ppa
-sudo apt-get update
-sudo apt-get install variety
-sudo apt-get update
+sudo apt update
+echo 1896 | sudo -S apt install wallch -y
