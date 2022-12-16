d92d2a446266bc93507e101dd0079af2f436a9fc
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-16 19:29:18 +0900

    Update first_221226.sh

--- first_221226.sh
+++ first_221226.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo installing package 21/03/27
-<< COMMENTOUT
-curl -sf https://raw.githubusercontent.com/Dash-2019/pc_setup/master/Ubuntu20_Apps.sh | sh -s
-sudo apt update
-COMMENTOUT
 
+<< COMMENTOUT
 echo 1896 | sudo -S apt install openssh-server -y
 sudo systemctl start ssh
 sudo systemctl enable ssh
+
+COMMENTOUT
+
+# 壁紙チェンジャー
+sudo add-apt-repository ppa:peterlevi/ppa
+sudo apt-get update
+sudo apt-get install variety
+sudo apt-get update
