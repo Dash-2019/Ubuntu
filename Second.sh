b503cd3c741fb9317b1663c1be02031b7e4ec5ab
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-19 08:12:51 +0900

    Update Second.sh

--- Second.sh
+++ Second.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#curl -sf https://raw.githubusercontent.com/Dash-2019/Ubuntu/main/Second.sh | sh -s
 
 << COMMENTOUT
 #Atom, VScode, Sublime Text
@@ -12,3 +13,11 @@ COMMENTOUT
 #chrome -----------------------------------------
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
+
+#typora--------------------------------
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo apt-key add -
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt-get update
+sudo apt-get -y install typora
