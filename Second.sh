bad791ca0847b1f73abf7911206828d0e6138057
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-18 14:33:36 +0900

    Create Second.sh

---
+++ Second.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+<< COMMENTOUT
+#Atom, VScode, Sublime Text
+sudo apt install -y snapd snapd-xdg-open
+sudo snap install sublime-text --classic
+
+sudo apt update && sudo apt install -y telegram-desktop
+
+COMMENTOUT
