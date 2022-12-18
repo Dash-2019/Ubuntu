ac27ec1fb2c622290e4b791d4afeec8dfcf19182
Haru <46810488+Dash-2019@users.noreply.github.com>
2022-12-19 08:09:41 +0900

    Update Second.sh
    chrome

--- Second.sh
+++ Second.sh
@@ -8,3 +8,7 @@ sudo snap install sublime-text --classic
 sudo apt update && sudo apt install -y telegram-desktop
 
 COMMENTOUT
+
+#chrome -----------------------------------------
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
